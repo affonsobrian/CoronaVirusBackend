f9f7abe1c9ae48da0ab438a5bb0aa7ba13f93b41
Affonso Azevedo <contato.affonsobrian@gmail.com>
2020-05-30 21:57:11 -0300

    Add ranking

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     address = models.TextField(max_length=255, blank=False, null=True)
